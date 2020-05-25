99957ef726625e9e998d1d4d327d2ecd18224c14
Alexander Chori <alexandrchori@gmail.com>
2020-05-25 21:54:45 +0300

    Done

--- src/main/java/ru/itmo/softwaredesign/EmitLog.java
+++ src/main/java/ru/itmo/softwaredesign/EmitLog.java
@@ -1,26 +1,95 @@
 package ru.itmo.softwaredesign;
 
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import org.jetbrains.annotations.NotNull;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+import java.util.StringJoiner;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public class EmitLog {
-    private static final String EXCHANGE_NAME = "logs";
+    private static final String EXCHANGE_NAME = "router";
+    private static final String EXCHANGE_NAME_2 = "logs_3";
+    private static final String EXCHANGE_NAME_3 = "logs_3_out";
 
-    public static void main(String[] argv) throws Exception {
+    public static void main(@NotNull String[] argv) throws Exception {
+        final Scanner scanner = new Scanner(System.in);
+
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
+        factory.setPort(32769);
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
+            String username = argv.length < 1 ? "guest" : argv[0];
+            String channelKey = argv.length < 2 ? "main" : argv[1];
+
+            String packedMessage = new StringJoiner(":").add(username).add(channelKey).toString();
+
             channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
 
-            String message = argv.length < 1 ? "info: Hello World!" :
-                    String.join(" ", argv);
+            /* Subscribe to reply back */
+            String callbackQueue = channel.queueDeclare().getQueue();
+            channel.queueBind(callbackQueue, EXCHANGE_NAME, callbackQueue);
+            final BlockingQueue<String> response = new ArrayBlockingQueue<>(1);
 
-            channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes(StandardCharsets.UTF_8));
-            System.out.println(" [x] Sent '" + message + "'");
+            channel.basicConsume(callbackQueue, true, (tag, delivery) -> {
+                System.out.println("Welcome to " + channelKey + ", " + username + '!');
+
+                channel.exchangeDeclare(EXCHANGE_NAME_2, "direct");
+
+                /* Show all messages */
+                final BlockingQueue<String> messages = new ArrayBlockingQueue<>(1);
+
+                Channel outChannel = connection.createChannel();
+                outChannel.exchangeDeclare(EXCHANGE_NAME_3, "direct");
+                String messagesQueue = channel.queueDeclare().getQueue();
+                outChannel.queueBind(messagesQueue, EXCHANGE_NAME_3, channelKey);
+                outChannel.basicConsume(messagesQueue, true, (innerTag, innerDelivery) -> {
+                    System.out.println(new String(innerDelivery.getBody(), StandardCharsets.UTF_8));
+                }, consumerTag -> {
+                });
+
+                Runnable r = () -> {
+                    while (true) {
+                        try {
+                            System.out.println(messages.take());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                Thread t = new Thread(r);
+                t.start();
+
+                while (true) {
+                    String message = new StringJoiner(":").add(username).add(scanner.nextLine()).toString();
+
+                    channel.basicPublish(
+                            EXCHANGE_NAME_2,
+                            channelKey,
+                            null,
+                            message.getBytes(StandardCharsets.UTF_8)
+                    );
+                }
+            }, consumerTag -> {
+                System.out.println("Cancel?");
+            });
+
+            /* Send welcoming message */
+            channel.basicPublish(
+                    EXCHANGE_NAME,
+                    "",
+                    new AMQP.BasicProperties.Builder().replyTo(callbackQueue).build(),
+                    packedMessage.getBytes(StandardCharsets.UTF_8)
+            );
+
+            /* Blocking wait for answer */
+            System.out.println(response.take());
         }
     }
 }
--- src/main/java/ru/itmo/softwaredesign/ReceiveLogs.java
+++ src/main/java/ru/itmo/softwaredesign/ReceiveLogs.java
@@ -8,11 +8,14 @@ import com.rabbitmq.client.DeliverCallback;
 import java.nio.charset.StandardCharsets;
 
 public class ReceiveLogs {
-    private static final String EXCHANGE_NAME = "logs";
+    private static final String EXCHANGE_NAME = "router";
+    private static final String EXCHANGE_NAME_2 = "logs_3";
+    private static final String EXCHANGE_NAME_3 = "logs_3_out";
 
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
+        factory.setPort(32769);
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
@@ -23,9 +26,62 @@ public class ReceiveLogs {
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
-            System.out.println(" [x] Received '" + message + "'");
+            String[] message = new String(delivery.getBody(), StandardCharsets.UTF_8).split(":");
+
+            if (message.length < 2) {
+                return;
+            }
+
+            String username = message[0];
+            String channelName = message[1];
+
+            System.out.println(username + " joined channel " + channelName);
+
+            /* Queue for incoming messages */
+            channel.exchangeDeclare(EXCHANGE_NAME_2, "direct");
+            String anotherQueueName = channel.queueDeclare().getQueue();
+            channel.queueBind(anotherQueueName, EXCHANGE_NAME_2, channelName);
+
+            /* Queue for broadcasting channel */
+            Channel outChannel = connection.createChannel();
+            outChannel.exchangeDeclare(EXCHANGE_NAME_3, "direct");
+            String outQueueName = channel.queueDeclare().getQueue();
+            outChannel.queueBind(outQueueName, EXCHANGE_NAME_3, channelName);
+
+            DeliverCallback anotherDeliverCallback = (innerConsumerTag, innerDelivery) -> {
+                String[] messageParts = new String(innerDelivery.getBody(), StandardCharsets.UTF_8).split(":");
+
+                if (messageParts.length < 2 || !messageParts[0].equals(username)) {
+                    return;
+                }
+
+                String innerMessage = messageParts[1];
+                String formattedMessage = '[' + username + "] " + innerMessage;
+
+                System.out.println(
+                        " [x] Received " + formattedMessage + " (" + innerDelivery.getEnvelope().getRoutingKey() + ')'
+                );
+
+                outChannel.basicPublish(
+                        EXCHANGE_NAME_3,
+                        channelName,
+                        null,
+                        formattedMessage.getBytes(StandardCharsets.UTF_8)
+                );
+            };
+            channel.basicConsume(anotherQueueName, true, anotherDeliverCallback, innerConsumerTag -> {
+            });
+
+            String response = "Welcome to " + channelName + ", " + username + '!';
+            channel.basicPublish(
+                    EXCHANGE_NAME,
+                    delivery.getProperties().getReplyTo(),
+                    null,
+                    response.getBytes(StandardCharsets.UTF_8)
+            );
         };
-        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
+
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
+        });
     }
 }
